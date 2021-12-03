4e2607bea528b7d01572064e1d445feb0a0b7325
chauhanprakhar <201951116@iiitvadodara.ac.in>
2021-12-03 10:16:57 +0530

    transaction error

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545")
 var web3 = new Web3(provider)
 var ownerAddress = "0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c"
 var userAddress = "0xf60c2Ea62EDBfE808163751DD0d8693DCb30019c"
-var assetAddress = "0x6b175474e89094c44da98b954eedeac495271d0f"
+var assetAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
 var tokenAddress = TokenAbi.address;
-console.log(tokenAddress)
 var ethWeiDecimals;
-amountToSwap = 10
+amountToSwap = 1
 allowedAmount = 12
 amountWithdraw = 9
 var TokenContract = new web3.eth.Contract(TokenAbi.abi, tokenAddress);
@@ -22,9 +21,12 @@ var AssetContract = new web3.eth.Contract(erc20Abi, assetAddress);
 async function driverCode(){
     const decimal = await AssetContract.methods.decimals().call()
     ethWeiDecimals = parseInt(decimal);
-    amountToSwapWei = new BigNumber(amountToSwap).shiftedBy(ethWeiDecimals);
-    amountToAllow = new BigNumber(allowedAmount).shiftedBy(ethWeiDecimals);
-    amountToWithdraw = new BigNumber(amountWithdraw).shiftedBy(ethWeiDecimals);
+    console.log(ethWeiDecimals)
+    var amountToSwapWei = new BigNumber(amountToSwap).shiftedBy(ethWeiDecimals);
+    var amountToAllow = new BigNumber(allowedAmount).shiftedBy(ethWeiDecimals);
+    var amountToWithdraw = new BigNumber(amountWithdraw).shiftedBy(ethWeiDecimals);
+    // var amountToSwapWei = "1000000"
+    // var amountToAllow = "13000000"
     var assetBalance = await AssetContract.methods.balanceOf(userAddress).call();
     console.log(amountToSwapWei)
     console.log(amountToAllow)
@@ -33,6 +35,16 @@ async function driverCode(){
     approveSpender()
 
     async function approveSpender() {
+
+        var bal = await TokenContract.methods
+          .balance().call()
+        console.log("current balance is " + bal)
+
+        var award = await TokenContract.methods
+        .awardBalance().call()
+
+      console.log("current Award balance is " + award)
+
         await AssetContract.methods
           .approve(tokenAddress, amountToAllow)
           .send({ from: ownerAddress }, async (err, tx) => {
